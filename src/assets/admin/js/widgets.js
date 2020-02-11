ac710d82d8b833307ae420dd10d4ce9f90f118d4
Akshit Sethi <akshitsethi@gmail.com>
2020-02-11 20:44:49 +0530

    Filter the JS widgets script

--- src/assets/admin/js/widgets.js
+++ src/assets/admin/js/widgets.js
@@ -17,16 +17,16 @@
 		} );
 
 
-		// Personal Widget
-		// -------------------------------------------------
-
+		/**
+		 * Personal widget.
+		 */
 		$( document ).on( 'click', '#as-wb-personal-btn', function( e ) {
 			e.preventDefault();
 
 			var custom_uploader;
 			var attachment;
 
-			// Media Uploader Object
+			// Media uploader.
 			if( custom_uploader ) {
 				custom_uploader.open();
 				return;
@@ -40,7 +40,7 @@
 				multiple: false
 			} );
 
-			// Paste URL to Text Field
+			// Paste URL to text field.
 			custom_uploader.on( 'select', function() {
 				attachment = custom_uploader.state().get( 'selection' ).first().toJSON();
 
@@ -48,15 +48,15 @@
 				$( '.as-wb-personal-upload .as-wb-personal-preview' ).html( '<img src="' + attachment.url + '" />' );
 				$( '.as-wb-personal-append' ).html( '<a href="javascript:;" id="as-wb-personal-remove">' + as_wb_js.remove_text + '</a>' );
 
-				// Change State
+				// Change state.
 				$( '.as-wb-personal-upload' ).closest( '.widget' ).find( 'input[type="submit"]' ).val( as_wb_js.save_text ).prop( 'disabled', false );
 			} );
 
-			// Open Dialog
+			// Open dialog.
 			custom_uploader.open();
 		} );
 
-		// Remove Option
+		// Remove option.
 		$( document ).on( 'click', '#as-wb-personal-remove', function( e ) {
 			e.preventDefault();
 
@@ -64,21 +64,21 @@
 			$( '.as-wb-personal-upload .as-wb-personal-preview' ).html( as_wb_js.image_preview_text );
 			$( this ).hide();
 
-			// Change State
+			// Change state.
 			$( '.as-wb-personal-upload' ).closest( '.widget' ).find( 'input[type="submit"]' ).val( as_wb_js.save_text ).prop( 'disabled', false );
 		} );
 
 
-		// Ads Widget
-		// -------------------------------------------------
-
+		/**
+		 * Ads Widget.
+		 */
 		$( document ).on( 'click', '#as-wb-ads-btn', function( e ) {
 			e.preventDefault();
 
 			var custom_uploader;
 			var attachment;
 
-			// Media Uploader Object
+			// Media uploader.
 			if( custom_uploader ) {
 				custom_uploader.open();
 				return;
@@ -92,7 +92,7 @@
 				multiple: false
 			} );
 
-			// Paste URL to Text Field
+			// Paste URL to text field.
 			custom_uploader.on( 'select', function() {
 				attachment = custom_uploader.state().get( 'selection' ).first().toJSON();
 
@@ -100,15 +100,15 @@
 				$( '.as-wb-ads-upload .as-wb-ads-preview' ).html( '<img src="' + attachment.url + '" />' );
 				$( '.as-wb-ads-append' ).html( '<a href="javascript:;" id="as-wb-ads-remove">' + as_wb_js.remove_text + '</a>' );
 
-				// Change State
+				// Change state.
 				$( '.as-wb-ads-upload' ).closest( '.widget' ).find( 'input[type="submit"]' ).val( as_wb_js.save_text ).prop( 'disabled', false );
 			} );
 
-			// Open Dialog
+			// Open dialog.
 			custom_uploader.open();
 		} );
 
-		// Remove Option
+		// Remove option.
 		$( document ).on( 'click', '#as-wb-ads-remove', function( e ) {
 			e.preventDefault();
 
@@ -116,31 +116,10 @@
 			$( '.as-wb-ads-upload .as-wb-ads-preview' ).html( as_wb_js.ad_preview_text );
 			$( this ).hide();
 
-			// Change State
+			// Change state.
 			$( '.as-wb-ads-upload' ).closest( '.widget' ).find( 'input[type="submit"]' ).val( as_wb_js.save_text ).prop( 'disabled', false );
 		} );
 
-
-		// Notices & Registration
-		// -------------------------------------------------
-		// AJAX Call
-
-		function as_remove_options( target, action ) {
-			$( document ).on( 'click', target, function( e ) {
-				e.preventDefault();
-
-				$.ajax( {
-					type: 'POST',
-					url: ajaxurl,
-					data: {
-						action: action
-					}
-				} );
-			} );
-		}
-
-		as_remove_options( '.as-wb-offer .notice-dismiss', 'as_wb_remove_offer' );
-		as_remove_options( '.as-wb-register .notice-dismiss', 'as_wb_remove_reg' );
 	} );
 
 })( jQuery );
