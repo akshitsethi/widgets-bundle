25b863350b861d11c09cb0366b511776803199ad
Akshit Sethi <akshitsethi@gmail.com>
2020-03-23 23:22:49 +0530

    Added AJAX functions for processing options and support request

--- src/inc/class-admin.php
+++ src/inc/class-admin.php
@@ -18,6 +18,8 @@ namespace AkshitSethi\Plugins\WidgetsBundle {
 		public function __construct() {
 			add_action( 'admin_menu', [ $this, 'add_menu' ] );
 			add_action( 'admin_enqueue_scripts', [ $this, 'widget_scripts' ] );
+			add_action( 'wp_ajax_' . Config::PREFIX . 'support', [ $this, 'support_ticket' ] );
+			add_action( 'wp_ajax_' . Config::PREFIX . 'options', [ $this, 'save_options' ] );
 
 			add_filter( 'plugin_row_meta', [ $this, 'meta_links' ], 10, 2 );
 		}
@@ -108,6 +110,85 @@ namespace AkshitSethi\Plugins\WidgetsBundle {
 
 			return $links;
 		}
+
+
+		/**
+		 * Processes plugin options via an AJAX call.
+		 */
+		public function save_options() {
+			// Storing response in an array
+			$response = [
+				'code' 			=> 'success',
+				'response' 	=> esc_html__( 'Options have been updated successfully.', 'widgets-bundle' )
+			];
+
+			// Filter and sanitize
+			$options['ads'] 			= isset( $_POST['as_wb_ads'] ) 				? true : false;
+			$options['personal'] 	= isset( $_POST['as_wb_personal'] ) 	? true : false;
+			$options['posts'] 		= isset( $_POST['as_wb_posts'] ) 			? true : false;
+			$options['quote'] 		= isset( $_POST['as_wb_quote'] ) 			? true : false;
+			$options['social'] 		= isset( $_POST['as_wb_social'] ) 		? true : false;
+			$options['subscribe'] = isset( $_POST['as_wb_subscribe'] ) 	? true : false;
+			$options['instagram'] = isset( $_POST['as_wb_instagram'] ) 	? true : false;
+			$options['facebook'] 	= isset( $_POST['as_wb_facebook'] ) 	? true : false;
+			$options['twitter'] 	= isset( $_POST['as_wb_twitter'] ) 		? true : false;
+
+			// Update options
+			update_option( 'as_wb_options', $options );
+
+			// Headers for JSON format
+			header( "Content-Type: application/json" );
+			echo json_encode( $response );
+
+			// Exit
+			// For AJAX functions
+			exit();
+		}
+
+
+		/**
+		 * Creates support ticket via the options panel.
+		 */
+		public function support_ticket() {
+			// Storing response in an array
+			$response = [
+				'code' 			=> 'error',
+				'response' 	=> esc_html__( 'Please fill in both the fields to create your support ticket.', 'widgets-bundle' )
+			];
+
+			// Filter and sanitize
+			if ( ! empty( $_POST['as_support_email'] ) && ! empty( $_POST['as_support_issue'] ) ) {
+				$admin_email 	= sanitize_text_field( $_POST['as_support_email'] );
+				$issue 				= htmlentities( $_POST['as_support_issue'] );
+				$subject 			= '[Widgets Bundle v' . Config::VERSION . '] by ' . $admin_email;
+				$body 				= "Email: $admin_email \r\nIssue: $issue";
+				$headers 			= 'From: ' . $admin_email . "\r\n" . 'Reply-To: ' . $admin_email;
+
+				// Send email
+				if ( wp_mail( 'akshitsethi@gmail.com', $subject, $body, $headers ) ) {
+					// Success
+					$response = [
+						'code' 			=> 'success',
+						'response' 	=> esc_html__( 'I have received your support ticket and will get back to you shortly!', 'widgets-bundle' )
+					];
+				} else {
+					// Failure
+					$response = [
+						'code' 			=> 'error',
+						'response' 	=> esc_html__( 'There was an error creating the support ticket. You can try again later or send me an email directly at akshitsethi@gmail.com', 'widgets-bundle' )
+					];
+				}
+			}
+
+			// Headers for JSON format
+			header( "Content-Type: application/json" );
+			echo json_encode( $response );
+
+			// Exit
+			// For AJAX functions
+			exit();
+		}
+
 
 		/**
 		 * Displays settings page for the plugin.
