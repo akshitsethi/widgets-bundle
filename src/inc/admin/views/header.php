f541e499a8e1448d27f3a25b4a8ba44c4dfb5dbe
Akshit Sethi <akshitsethi@gmail.com>
2020-04-08 05:56:41 +0530

    Generic changes to the header.php file

--- src/inc/admin/views/header.php
+++ src/inc/admin/views/header.php
@@ -12,13 +12,13 @@ use AkshitSethi\Plugins\WidgetsBundle\Config;
 
 <div class="as-cnt-fix">
 	<div class="as-header as-clearfix">
-		<img src="<?php echo Config::$plugin_url; ?>assets/admin/images/lrg-icon.png" alt="<?php echo esc_attr_e( 'Widgets Bundle', 'widgets-bundle' ); ?>" class="as-logo">
+		<img src="<?php echo Config::$plugin_url; ?>assets/admin/images/lrg-icon.png" alt="<?php echo Config::get_plugin_name(); ?>" class="as-logo">
 		<p>
-			<strong><?php esc_html_e( 'Widgets Bundle', 'widgets-bundle' ); ?></strong>
+			<strong><?php echo Config::get_plugin_name(); ?></strong>
 			<span><?php esc_html_e( 'by', 'widgets-bundle' ); ?> <a href="https://akshitsethi.com/" target="_blank"><?php esc_html_e( 'Akshit Sethi', 'widgets-bundle' ); ?></a></span>
 		</p>
 
 		<div class="as-header-right">
-			<input type="submit" id="<?php echo Config::PREFIX . 'submit'; ?>" name="<?php echo Config::PREFIX . 'submit'; ?>" class="as-btn" value="<?php esc_html_e( 'Save Changes', 'widgets-bundle' ); ?>" data-tab="#options">
+			<input type="submit" id="<?php echo Config::PREFIX . 'submit'; ?>" name="<?php echo Config::PREFIX . 'submit'; ?>" class="as-btn" value="<?php esc_attr_e( 'Save Changes', 'widgets-bundle' ); ?>" data-tab="#options">
 		</div><!-- .as-header-right -->
 	</div><!-- .as-header -->
